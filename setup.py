9893e21b3e19f9176f56fb4d5acd8fef79248f3c
rookieeew <tong.wu2@ucdconnect.ie>
2023-01-26 12:13:03 +0000

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-print("hello world!")
-for i in range(10):
-    print(i)
+def BubbleSort(list):
+    for i in range(len(list) - 1):
+        for j in range(len(list) - 1 - i):
+            if list[j] > list[j + 1]:
+                tem = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = tem
 
-print("ok")
-print("It is done.")
+    return list
+
+
+list1 = [10, 5, 6, 3, 7]
+print(BubbleSort(list1))
+print("hahhhahaha")
